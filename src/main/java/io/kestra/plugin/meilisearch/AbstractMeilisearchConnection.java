f69f4d6fb6f3ca8e247aaa2a09889ec8c3537b6c
Mathieu Gabelle <mgabelle@kestra.io>
2024-09-09 11:41:10 +0200

    feat(property v2): implement v2 propery for DocumentGet
    Refactor MeilisearchConnectionInterface for v2
    
    Refactor DocumentGet for v2 property

--- src/main/java/io/kestra/plugin/meilisearch/AbstractMeilisearchConnection.java
+++ src/main/java/io/kestra/plugin/meilisearch/AbstractMeilisearchConnection.java
@@ -3,6 +3,7 @@ package io.kestra.plugin.meilisearch;
 import com.meilisearch.sdk.Client;
 import com.meilisearch.sdk.Config;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
 import lombok.*;
@@ -14,11 +15,11 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @NoArgsConstructor
 public abstract class AbstractMeilisearchConnection extends Task implements MeilisearchConnectionInterface {
-    private String url;
-    private String key;
+    protected Property<String> url;
+    protected Property<String> key;
 
     public Client createClient(RunContext runContext) throws IllegalVariableEvaluationException {
-        Config config = new Config(runContext.render(url), runContext.render(key));
+        Config config = new Config(url.as(runContext, String.class), key.as(runContext, String.class));
         return new Client(config);
     }
 }
