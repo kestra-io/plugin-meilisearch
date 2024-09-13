06a5e2de4b36931423f346330ab73a96deced8d1
Mathieu Gabelle <mgabelle@kestra.io>
2024-09-13 10:48:51 +0200

    doc: add package-info

---
+++ src/main/java/io/kestra/plugin/meilisearch/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Meilisearch collection database.\n" +
+        "Meilisearch is a RESTful search API. It aims to be a ready-to-go solution for everyone who wants a fast and relevant search experience for their end-users ",
+    categories = { PluginSubGroup.PluginCategory.DATABASE }
+)
+package io.kestra.plugin.meilisearch;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
