0d20bdd387f55f9aec31da5099c0e4e8b1b77dce
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:10:58 +0100

    chore: update plugin categories (#45)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/meilisearch/package-info.java
+++ src/main/java/io/kestra/plugin/meilisearch/package-info.java
@@ -1,7 +1,7 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Meilisearch collection database.\n" +
         "Meilisearch is a RESTful search API. It aims to be a ready-to-go solution for everyone who wants a fast and relevant search experience for their end-users ",
-    categories = { PluginSubGroup.PluginCategory.DATABASE }
+        categories = { PluginSubGroup.PluginCategory.BUSINESS }
 )
 package io.kestra.plugin.meilisearch;
 
