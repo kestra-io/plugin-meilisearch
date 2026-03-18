22c235022977df6cdd551b78e0aac53e285eff06
Pradumna Saraf <pradumnasaraf@gmail.com>
2026-03-18 11:42:04 +0530

    feat: add sanity checks and test

---
+++ src/test/java/io/kestra/plugin/meilisearch/RunnerTest.java
@@ -0,0 +1,21 @@
+package io.kestra.plugin.meilisearch;
+
+import io.kestra.core.junit.annotations.ExecuteFlow;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.flows.State;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest(startRunner = true)
+class RunnerTest {
+    @Test
+    @ExecuteFlow("sanity-checks/all_meilisearch.yaml")
+    void all_meilisearch(Execution execution) {
+        assertThat(execution.getTaskRunList(), hasSize(10));
+        assertThat(execution.getState().getCurrent(), is(State.Type.SUCCESS));
+    }
+}
